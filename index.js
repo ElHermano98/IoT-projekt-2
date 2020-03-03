40f2f8c182fd7ecd4f6bfef6612ca1f758bba79c
ElHermano98 <hermanabra@gmail.com>
2020-03-03 13:45:38 +0100

    IoT projekt 2222

---
+++ index.js
@@ -0,0 +1,77 @@
+// Based on an example:
+//https://github.com/don/cordova-plugin-ble-central
+
+
+// ASCII only
+function bytesToString(buffer) {
+    return String.fromCharCode.apply(null, new Uint8Array(buffer));
+}
+
+// ASCII only
+function stringToBytes(string) {
+    var array = new Uint8Array(string.length);
+    for (var i = 0, l = string.length; i < l; i++) {
+        array[i] = string.charCodeAt(i);
+    }
+    return array.buffer;
+}
+
+
+
+//the bluefruit UART Service
+var blue ={
+	serviceUUID: '6e400001-b5a3-f393-e0a9-e50e24dcca9e',
+    txCharacteristic: '6e400002-b5a3-f393-e0a9-e50e24dcca9e', // transmit is from the phone's perspective
+    rxCharacteristic: '6e400003-b5a3-f393-e0a9-e50e24dcca9e'  // receive is from the phone's perspective
+}
+
+var ConnDeviceId;
+var bleDeviceName;
+var deviceList =[];
+
+setTimeout("window.location.reload();",20000); //reload siden hvert 20. sekund. Dermed genindlæses Bluetooth-liste
+ 
+function onLoad(){
+	document.addEventListener('deviceready', onDeviceReady, false);
+    bleDeviceList.addEventListener('touchstart', conn, false); // assume not scrolling
+}
+
+function onDeviceReady(){
+	refreshDeviceList();
+}
+
+	 
+function onDiscoverDevice(device){
+	//Make a list in html and show devises
+		var listItem = document.createElement('li'),
+		html = device.name+ "," + device.id;
+		listItem.innerHTML = html;
+		document.getElementById("bleDeviceList").appendChild(listItem);
+}
+
+
+function conn(){
+	var  deviceTouch= event.srcElement.innerHTML;
+	document.getElementById("debugDiv").innerHTML =""; // empty debugDiv
+	var deviceTouchArr = deviceTouch.split(",");
+	ConnDeviceId = deviceTouchArr[1];
+	//document.getElementById("debugDiv").innerHTML += "<br>"+deviceTouchArr[0]+"<br>"+deviceTouchArr[1]; //for debug:
+	ble.connect(ConnDeviceId, onConnect, onConnError);
+ }
+
+function onError(reason)  {
+	alert("ERROR: " + reason); // real apps should use notification.alert
+}
+
+
+
+function test(){
+	var url='https://placering.000webhostapp.com';	
+	openBrowser(url);
+}
+
+function openBrowser(url) {
+   var target = '_blank';
+   var options = "location=no"
+   var ref = cordova.InAppBrowser.open(url, target, options);
+}
